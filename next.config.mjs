983bdf748d21cebd9b67b11c79428f0ab95b19e2
faratim <faratim@gmail.com>
2025-04-03 10:52:12 -0400

    more debugging

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Add this to tell Next.js about your functions directory
-  experimental: {
-    cloudflareWorkers: true,
-  },
 };
 
 export default nextConfig;
